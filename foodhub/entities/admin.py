abb0af1fa5ae08929f0560804d137c6de3f40b0f
Abhishek Ray <abhishek.ray@urbanpiper.com>
2020-09-30 14:50:08 +0530

    Registered App Models on Admin

--- foodhub/entities/admin.py
+++ foodhub/entities/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from entities.models import Merchant, Item, Store, Order
 # Register your models here.
+
+models_iterable = [Merchant, Item, Store, Order]
+
+admin.site.register(models_iterable)
