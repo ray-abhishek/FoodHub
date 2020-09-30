5ff3499f938ee50c91332d8c1db531c1e9af189d
Abhishek Ray <abhishek.ray@urbanpiper.com>
2020-09-30 14:51:45 +0530

    Added Validators to ensure Merchant Integrity while Order creation

---
+++ foodhub/entities/validators.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+
+def validate_store_merchant(data):
+    store_merchant_id = data['store'].merchant.id
+    order_merchant_id = data['merchant'].id
+    if store_merchant_id != order_merchant_id:
+        raise serializers.ValidationError(
+            {"store": "Merchant ID doesn't match with Order's Merchant."})
+
+
+def validate_items_merchant(data):
+    order_merchant_id = data['merchant'].id
+    item_merchant_ids = [item.merchant.id for item in data['items']]
+    for item_merchant_id in item_merchant_ids:
+        if item_merchant_id != order_merchant_id:
+            raise serializers.ValidationError(
+                {"items": "Item(s) do not belong to the Order's Merchant."})
