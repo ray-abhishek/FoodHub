b71f52553e09b8283eceb0241fc06782dbd48d5f
Abhishek Ray <abhishek.ray@urbanpiper.com>
2020-09-30 22:41:41 +0530

    Added Authentication Classes Django Rest Framework

--- foodhub/foodhub/settings.py
+++ foodhub/foodhub/settings.py
@@ -125,3 +125,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Django Rest Framework Settings
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+
+    ]
+}
