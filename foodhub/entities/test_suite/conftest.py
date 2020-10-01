d1f11ea9143008c627c5bf2652c8822c406d6106
Abhishek Ray <abhishek.ray@urbanpiper.com>
2020-10-02 00:15:22 +0530

    Added session scope for fixtures

--- foodhub/entities/test_suite/conftest.py
+++ foodhub/entities/test_suite/conftest.py
@@ -4,25 +4,41 @@ from rest_framework.test import APIRequestFactory
 from rest_framework.test import APIClient
 
 
-@pytest.fixture
-def test_user(db):
-    user = User.objects.create_user(username='admin', password='admin')
-    return user
+@pytest.fixture(scope="session")
+def test_user(django_db_blocker):
+
+    with django_db_blocker.unblock():
+        user = User.objects.create_user(
+            username='admin', password='admin')
+        print("\n Creating User \n")
+        yield user
 
 
-@pytest.fixture
+@ pytest.fixture(scope="session")
 def api_client():
+    print("returning APIClient()")
     return APIClient()
 
 
-@pytest.fixture
+"""@pytest.fixture
 def api_request_factory():
     factory = APIRequestFactory()
     return factory
+"""
 
 
-@pytest.fixture
-def api_client_with_credentials(api_client, test_user):
-    api_client.force_authenticate(user=test_user)
-    yield api_client
-    api_client.force_authenticate(user=None)
+@ pytest.fixture(scope="session")
+def api_client_with_credentials(django_db_blocker, api_client, test_user):
+
+    with django_db_blocker.unblock():
+        api_client.force_authenticate(user=test_user)
+        print("\nAuthenticating\n")
+        yield api_client
+        print("\nUnauthenticating\n")
+        api_client.force_authenticate(user=None)
+
+
+"""
+setup function - pytest
+django_db_blocker
+"""
