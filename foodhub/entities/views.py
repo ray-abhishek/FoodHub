a8fe8fdcc285e3531c58ace28b40b409422dd730
Abhishek Ray <abhishek.ray@urbanpiper.com>
2020-10-01 14:56:31 +0530

    Added detail-view for Items, Orders, Stores of Merchant

--- foodhub/entities/views.py
+++ foodhub/entities/views.py
@@ -1,10 +1,13 @@
 from rest_framework import viewsets
-
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from entities.models import (Merchant, Item, Store, Order)
 from entities.serializers import (MerchantSerializer, ItemSerializer,
                                   StoreSerializer, OrderSerializer)
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
+from django.core import serializers
+import json
 # Create your views here.
 
 
@@ -16,6 +19,27 @@ class MerchantViewSet(viewsets.ModelViewSet):
     queryset = Merchant.objects.all()
     authentication_classes = [BasicAuthentication]
     permission_classes = [IsAuthenticated]
+
+    @action(detail=True)
+    def stores(self, request, pk=None):
+        merchant = Merchant.objects.get(pk=pk)
+        stores = Store.objects.filter(merchant=merchant)
+        serializer = StoreSerializer(stores, many=True)
+        return Response(serializer.data)
+
+    @action(detail=True)
+    def items(self, request, pk=None):
+        merchant = Merchant.objects.get(pk=pk)
+        items = Item.objects.filter(merchant=merchant)
+        serializer = ItemSerializer(items, many=True)
+        return Response(serializer.data)
+
+    @action(detail=True)
+    def orders(self, request, pk=None):
+        merchant = Merchant.objects.get(pk=pk)
+        orders = Order.objects.filter(merchant=merchant)
+        serializer = OrderSerializer(orders, many=True)
+        return Response(serializer.data)
 
 
 class ItemViewSet(viewsets.ModelViewSet):
