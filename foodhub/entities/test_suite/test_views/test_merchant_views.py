73ff0b5011347be8932d997347c1fcc5fd26ed77
Abhishek Ray <abhishek.ray@urbanpiper.com>
2020-10-02 00:16:45 +0530

    Changed logic of merchant-list view

--- foodhub/entities/test_suite/test_views/test_merchant_views.py
+++ foodhub/entities/test_suite/test_views/test_merchant_views.py
@@ -1,9 +1,7 @@
 import pytest
 from django.urls import reverse
 import json
-from entities.test_suite.test_views.helper_functions import (
-    create_merchants, create_merchant
-)
+from entities.models import Merchant
 
 
 @pytest.mark.parametrize('name,email,phone,status_code', [
@@ -25,22 +23,28 @@ def test_merchant_creation(name, email, phone, status_code, api_client_with_cred
     assert response.status_code == status_code
 
 
-@pytest.mark.parametrize('number_of_merchants', [1, 3, 5])
-def test_merchant_listview(number_of_merchants, api_client_with_credentials):
-    create_merchants(number_of_merchants)
+@pytest.mark.django_db
+def test_merchant_listview(api_client_with_credentials):
+    merchants_in_db = Merchant.objects.all()
     url = reverse('merchant-list')
     response = api_client_with_credentials.get(url)
     merchant_list = json.loads(response.content)
-    assert len(merchant_list) == number_of_merchants
+    assert len(merchants_in_db) == len(merchant_list)
 
 
 @pytest.mark.parametrize('initial_details,updated_details', [({"name": "Merchant", "email": "", "phone": "9438155726"}, {"name": "Merchant", "email": "", "phone": "1111111111"})])
 def test_merchant_updation(initial_details, updated_details, api_client_with_credentials):
 
-    initial_merchant = create_merchant(initial_details)
-    url = reverse('merchant-list') + str(initial_merchant.id) + '/'
-    response = api_client_with_credentials.patch(url, data=updated_details)
+    creation_url = reverse('merchant-list')
+    creation_response = api_client_with_credentials.post(
+        creation_url, data=initial_details)
+    initial_merchant = json.loads(creation_response.content)
+
+    updation_url = reverse('merchant-list') + str(initial_merchant["pk"]) + '/'
+    response = api_client_with_credentials.patch(
+        updation_url, data=updated_details)
     updated_merchant = json.loads(response.content)
     updated_merchant.pop('pk', None)
     updated_merchant.pop('created_at', None)
+
     assert updated_details == updated_merchant
