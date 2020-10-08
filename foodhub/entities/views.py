ed3a341cd0c21b625969fe68876837dbc1adfa7b
Abhishek Ray <abhishek.ray@urbanpiper.com>
2020-10-08 13:27:03 +0530

    Optimised query

--- foodhub/entities/views.py
+++ foodhub/entities/views.py
@@ -8,6 +8,8 @@ from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from entities.tasks import create_order
 import structlog
+from silk.profiling.profiler import silk_profile
+
 # Create your views here.
 
 log = structlog.get_logger()
@@ -24,9 +26,10 @@ class MerchantViewSet(viewsets.ModelViewSet):
 
     def create(self, request, *args, **kwargs):
         response = super().create(request, *args, **kwargs)
-        log.msg('Create Merchant Request', req=request)
+        log.msg('Create Merchant Request', req=request.data)
         return response
 
+    # @silk_profile(name='stores')
     @action(detail=True)
     def stores(self, request, pk=None):
         merchant = Merchant.objects.filter(pk=pk).first()
@@ -34,6 +37,7 @@ class MerchantViewSet(viewsets.ModelViewSet):
         serializer = StoreSerializer(stores, many=True)
         return Response(serializer.data)
 
+    # @silk_profile(name='items_of_merchants')
     @action(detail=True)
     def items(self, request, pk=None):
         merchant = Merchant.objects.filter(pk=pk).first()
@@ -41,6 +45,7 @@ class MerchantViewSet(viewsets.ModelViewSet):
         serializer = ItemSerializer(items, many=True)
         return Response(serializer.data)
 
+    # @silk_profile(name='orders_of_merchants')
     @action(detail=True)
     def orders(self, request, pk=None):
         merchant = Merchant.objects.filter(pk=pk).first()
@@ -54,13 +59,13 @@ class ItemViewSet(viewsets.ModelViewSet):
     API view to perform CRUD operations on Item
     """
     serializer_class = ItemSerializer
-    queryset = Item.objects.all()
+    queryset = Item.objects.select_related('merchant').all()
     authentication_classes = [BasicAuthentication]
     permission_classes = [IsAuthenticated]
 
     def create(self, request, *args, **kwargs):
         response = super().create(request, *args, **kwargs)
-        log.msg('Create Item Request', req=request)
+        log.msg('Create Item Request', req=request.data)
         return response
 
 
@@ -69,15 +74,17 @@ class StoreViewSet(viewsets.ModelViewSet):
     API view to perform CRUD operations on Store
     """
     serializer_class = StoreSerializer
-    queryset = Store.objects.all()
+    queryset = Store.objects.select_related(
+        'merchant').prefetch_related('items')
     authentication_classes = [BasicAuthentication]
     permission_classes = [IsAuthenticated]
 
     def create(self, request, *args, **kwargs):
         response = super().create(request, *args, **kwargs)
-        log.msg('Create Store Request', req=request)
+        log.msg('Create Store Request', req=request.data)
         return response
 
+    @silk_profile(name='orders_of_store')
     @action(detail=True)
     def orders(self, request, pk=None):
         store = Store.objects.filter(pk=pk).first()
@@ -91,15 +98,23 @@ class OrderViewSet(viewsets.ModelViewSet):
     API view to perform CRUD operations on Order
     """
     serializer_class = OrderSerializer
-    queryset = Order.objects.all()
+    queryset = Order.objects.select_related(
+        'merchant', 'store').prefetch_related('items')
     authentication_classes = [BasicAuthentication]
     permission_classes = [IsAuthenticated]
 
     def create(self, request):
-        log.msg('Create Order Request', req=request)
+        log.msg('Create Order Request', req=request.data)
         serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        create_order.delay(serializer.data)
-        headers = self.get_success_headers(serializer.validated_data)
-        json_response = {"message": "Order is being placed"}
-        return Response(json_response, status=status.HTTP_201_CREATED, headers=headers)
+        if serializer.is_valid(raise_exception=True):
+            create_order.delay(serializer.data)
+            headers = self.get_success_headers(serializer.validated_data)
+            log.msg('Serializer Data', data=serializer.data,
+                    validated_data=serializer.validated_data)
+            response = {"message": "Order is being placed"}
+            return Response(response, status=status.HTTP_201_CREATED,
+                            headers=headers)
+        else:
+            response = {"message": "Order could not be placed."}
+            return Response(response, status=status.HTTP_400_BAD_REQUEST,
+                            headers=headers)
