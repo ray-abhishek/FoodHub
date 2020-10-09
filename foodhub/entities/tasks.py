e9cdcc6081ed3593f3a1226d18df271f10bf7669
Abhishek Ray <abhishek.ray@urbanpiper.com>
2020-10-09 07:57:57 +0530

    Removed zombie code.

--- foodhub/entities/tasks.py
+++ foodhub/entities/tasks.py
@@ -1,4 +1,4 @@
-from entities.models import Merchant, Item, Store, Order
+from entities.models import Merchant, Store, Order
 from celery.decorators import task
 import structlog
 
@@ -14,44 +14,7 @@ def create_order(data):
     store_instance = Store.objects.filter(pk=data['store']).first()
     items = data.pop('items')
     order_instance = Order.objects.create(
-        merchant=merchant_instance, store=store_instance, total_cost=data['total_cost'])
+        merchant=merchant_instance, store=store_instance,
+        total_cost=data['total_cost'])
     order_instance.items.set(items)
     log.msg('Order Created', orderID=order_instance.id)
-
-
-"""
-@task(name="create_random_orders")
-def create_random_orders(num_of_orders):
-    if num_of_orders < 0:
-        return
-    present, store, item = get_item_store()
-    if not present:
-        return
-    for _ in range(num_of_orders):
-        order = Order.objects.create(
-            merchant=item.merchant, store=store, total_cost=item.cost)
-        order.items.set([item])
-        print(order.id)
-
-
-def get_item():
-    items = Item.objects.all()
-    for item in items:
-        yield item
-
-
-def get_item_store():
-
-    item = get_item()
-    store = None
-    while True:
-        try:
-            current_item = next(item)
-            store = Store.objects.filter(
-                merchant=current_item.merchant).first()
-        except StopIteration:
-            return False, None, None
-        finally:
-            if store is not None:
-                return True, store, current_item
-"""
