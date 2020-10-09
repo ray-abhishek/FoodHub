1497ac1e116b2722f61a0c12f42b6a215836547e
Abhishek Ray <abhishek.ray@urbanpiper.com>
2020-10-09 07:20:43 +0530

    Added comments

--- foodhub/entities/test_suite/test_views/test_order_creation.py
+++ foodhub/entities/test_suite/test_views/test_order_creation.py
@@ -1,27 +1,32 @@
 import pytest
 import json
 from django.urls import reverse
+from celery.contrib.pytest import celery_session_app, celery_session_worker
 
 
-@pytest.mark.parametrize('merchant_data,item_data,store_data,order_data,status_code', [({"name": "Merchant", "phone": 9428155726}, {"name": "TestFoodItem", "cost": 100}, {"name": "TestStore"}, {"total_cost": 100}, 201)])
+@pytest.mark.parametrize('merchant_data,item_data,store_data,order_data,status_code', [({"name": "TestMerchant", "phone": 9428155726}, {"name": "TestFoodItem", "cost": 100}, {"name": "TestStore"}, {"total_cost": 100}, 201)])
 def test_order_creation(merchant_data, item_data, store_data, order_data, status_code, api_client_with_credentials):
 
+    # Create a merchant
     merchant_url = reverse('merchant-list')
     response = api_client_with_credentials.post(
         merchant_url, data=merchant_data)
     merchant = json.loads(response.content)
 
+    # Create an item for above merchant
     item_data["merchant"] = merchant["pk"]
     item_url = reverse('item-list')
     response = api_client_with_credentials.post(item_url, data=item_data)
     item = json.loads(response.content)
 
+    # Create a store with above merchant, item
     store_data["merchant"] = merchant["pk"]
     store_data["items"] = item["pk"]
     store_url = reverse('store-list')
     response = api_client_with_credentials.post(store_url, data=store_data)
     store = json.loads(response.content)
 
+    # Place an order with above merchant, item, store
     order_data["merchant"] = merchant["pk"]
     order_data["store"] = store["pk"]
     order_data["items"] = item["pk"]
