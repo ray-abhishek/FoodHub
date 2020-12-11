f3e87d979678424cb0518ac99fe4d66ec9f4d1a8
Abhishek Ray <abhishek.ray@urbanpiper.com>
2020-12-11 10:17:30 +0530

    Update deploy_commands.sh

--- foodhub/deploy_commands.sh
+++ foodhub/deploy_commands.sh
@@ -1,5 +1,1 @@
-cd foodhub
-git pull origin ${BRANCH}
-source /opt/foodhub/bin/activate
-sudo docker-compose build server
-sudo docker-compose down && sudo docker-compose up
+docker-compose down && sudo docker-compose up
