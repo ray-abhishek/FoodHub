bc00c5cff8697a3439a0ce93710b3a5f374918a3
Abhishek Ray <abhishek.ray@urbanpiper.com>
2020-12-11 16:53:01 +0530

    Update docker-entrypoint.sh

--- foodhub/docker-entrypoint.sh
+++ foodhub/docker-entrypoint.sh
@@ -1,14 +1,5 @@
- #!/bin/sh
- set -e
+#!/bin/bash
 
- ##
- # Run a command to start python runserver
- #
- if [ $1 ];then
-     # If we passed a command, run it
-     exec "$@"
- else
-     # Otherwise start python runserver
-     python manage.py runserver 0.0.0.0:8000
- fi
- 
+# Start server
+echo "Starting server"
+python3 manage.py runserver 0.0.0.0:8000
