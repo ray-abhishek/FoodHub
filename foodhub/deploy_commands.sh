5b25610b2b07b3e18f4436f83988b4bc3d8c75d3
Abhishek Ray <abhishek.ray@urbanpiper.com>
2020-12-15 12:55:28 +0530

    .

--- foodhub/deploy_commands.sh
+++ foodhub/deploy_commands.sh
@@ -3,7 +3,7 @@ echo $1
 CONTAINS_MIGRATION='true'
 docker-compose build server
 docker-compose down && docker-compose up -d
-if CONTAINS_MIGRATION
+if $CONTAINS_MIGRATION
 then 
     docker-compose run --rm --entrypoint "python3 manage.py migrate" server
 fi
