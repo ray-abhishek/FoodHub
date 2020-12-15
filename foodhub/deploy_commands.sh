3e4ef5823835adf52a5c1dacb03bb179bafb1f01
Abhishek Ray <abhishek.ray@urbanpiper.com>
2020-12-15 13:11:52 +0530

    .

--- foodhub/deploy_commands.sh
+++ foodhub/deploy_commands.sh
@@ -1,6 +1,7 @@
 echo $0
 echo $1
-CONTAINS_MIGRATION='true'
+CONTAINS_MIGRATION=$1
+echo $CONTAINS_MIGRATION
 docker-compose build server
 docker-compose down && docker-compose up -d
 if $CONTAINS_MIGRATION
