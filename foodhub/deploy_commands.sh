3bb389a5e5b8b1ebb452123fcb8f7d8d1c38ba36
Abhishek Ray <abhishek.ray@urbanpiper.com>
2020-12-17 15:44:14 +0530

    .

--- foodhub/deploy_commands.sh
+++ foodhub/deploy_commands.sh
@@ -1,9 +1,7 @@
 echo $0
 CONTAINS_MIGRATION=$1
 echo $CONTAINS_MIGRATION
-git clone git@github.com:ray-abhishek/Foodhub.git
 git pull origin master
-ls
 docker-compose build server
 docker-compose down && docker-compose up -d
 if [ $CONTAINS_MIGRATION == "true" ]
